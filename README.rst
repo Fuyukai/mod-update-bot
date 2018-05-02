e3feb5f3e4c33dd5cf8ccf4f134a6371bf568aa6
Laura F. D <l@veriny.tf>
2018-05-02 12:35:26 +0100

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,18 @@
+fcbot
+-----
+
+A very basic bot used to notify of mod updates for the Factorio Discord.
+
+Usage
+=====
+
+ 1. ``pipenv install``
+
+ 2. Edit the channel ID in bot.py
+
+ 2. ``python bot.py <token>``
+
+Licence
+=======
+
+This bot is licenced under the GNU Afferro General Public Licence.
---
+++ bot.py
@@ -0,0 +1,138 @@
+import sys
+import time
+
+import curio
+import logging
+import requests
+from curio.thread import AWAIT, async_thread
+from curious.core.httpclient import HTTPClient
+from typing import Dict
+from urllib.parse import quote
+
+
+@async_thread()
+def _make_request(self, *args, **kwargs):
+    """
+    Makes a request via the current session.
+
+    :returns: The response body.
+    """
+    headers = kwargs.get("headers", None)
+    if headers is not None:
+        headers.update(self.headers.copy())
+    else:
+        headers = self.headers.copy()
+
+    # ensure path is escaped
+    path = kwargs["path"]
+    path = quote(path)
+    kwargs["path"] = path
+
+    url = "https://discordapp.com/api/v7" + kwargs.pop("path")
+
+    return requests.request(*args, headers=headers, url=url, **kwargs)
+
+
+# monkeypatch to use requests.
+HTTPClient._make_request = _make_request
+http_client = HTTPClient(token=sys.argv[1])
+# manager = CommandsManager(client)
+# manager.register_events()
+
+# state
+populating = True
+_running = False
+mods: Dict[str, str] = {}
+MODS_URL = "https://mods.factorio.com/api/mods"
+
+# channel IDs
+upd_channel = 390979829907980289
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@async_thread()
+def scrape_mod_portal():
+    global populating
+
+    while True:
+        headers = {"user-agent": f"factorio mod notifier/1.3"}
+        params = {"page_size": "max"}
+
+        for try_ in range(0, 10):
+            print("Scanning mods...")
+            page = requests.get(MODS_URL, params=params, headers=headers)
+
+            # went a page too far
+            if page.status_code == 404:
+                body = {"results": []}
+                break
+
+            if page.status_code != 200:
+                print("Mod portal died, sleeping and retrying in a bit")
+                time.sleep(5)
+                continue
+            else:
+                body = page.json()
+                break
+        else:
+            print("Could not download mod portal data at all. Retrying later.")
+            time.sleep(300)
+            continue
+
+        already_processed = set()
+        print("Processing mods...")
+        for mod in body.get("results", []):
+            name = mod["name"]
+
+            # Duplicate checking...
+            if name in already_processed:
+                continue
+
+            already_processed.add(name)
+            latest = mod["latest_release"]
+
+            # Check for new mods.
+            if name not in mods:
+                mods[name] = latest["version"]
+                if not populating:
+                    print("New mod:", name)
+                    AWAIT(_do_send_new(mod))
+                else:
+                    print("Populating mod:", name)
+            else:
+                if mods[name] != latest["version"]:
+                    print("Updated mod:", name)
+                    mods[name] = latest["version"]
+                    AWAIT(_do_send_update(mod))
+
+        # unflip populating because we're successfully downloaded all mods
+        populating = False
+        print("Waiting 60 seconds to send mod data.")
+        time.sleep(60)
+
+
+async def _do_send_new(mod: dict):
+    """
+    Sends a NEW mod message.
+    """
+    modname = quote(mod['name'])
+    url = f"https://mods.factorio.com/mods/{mod['owner']}/{modname}"
+    msg = f"**New mod:** {mod['title']} by {mod['owner']} - <{url}>"
+
+    await http_client.send_message(channel_id=upd_channel, content=msg)
+
+
+async def _do_send_update(mod: dict):
+    """
+    Sends an UPDATE mod message.
+    """
+    modname = quote(mod['name'])
+    url = f"https://mods.factorio.com/mods/{mod['owner']}/{modname}"
+    msg = f"**Updated mod:** {mod['title']} (new: **{mod['latest_release']['version']}**) " \
+          f"by {mod['owner']} - {url}"
+
+    await http_client.send_message(channel_id=upd_channel, content=msg)
+
+
+curio.run(scrape_mod_portal)
